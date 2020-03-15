8490d5822959341fb36124e0ceb02f8f5677edb1
b3ni <brarcos@gmail.com>
2020-03-15 12:31:10 +0100

    change column

--- database/migrations/2020_03_15_121000_size_error.php
+++ database/migrations/2020_03_15_121000_size_error.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class LogErrorsFrontendTables extends Migration
+class SizeError extends Migration
 {
     /**
      * Run the migrations.
