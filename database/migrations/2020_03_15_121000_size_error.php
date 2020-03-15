ebaeddc19fa9470345dfb9965bd1ff3b5c28ab7d
b3ni <brarcos@gmail.com>
2020-03-15 12:18:32 +0100

    change column

---
+++ database/migrations/2020_03_15_121000_size_error.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class LogErrorsFrontendTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('errors_frontend', function (Blueprint $table) {
+            $table->text('error')->change();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('errors_frontend', function (Blueprint $table) {
+            $table->string('error')->change();
+        });
+    }
+}
